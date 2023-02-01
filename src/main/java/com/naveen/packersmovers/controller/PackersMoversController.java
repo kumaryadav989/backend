c241ec1b4f83fb279a0208b6cde89b68450fad44
kumaryadav989 <99376581+kumaryadav989@users.noreply.github.com>
2023-02-02 00:15:58 +0530

    Update PackersMoversController.java

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -30,7 +30,7 @@ public class PackersMoversController {
         
         
         @GetMapping("/ping")
-        public String ping() {  return "The current batch is batch11" ;
+        public String ping() {  return "The current batch is batch12" ;
         			
         }
 
